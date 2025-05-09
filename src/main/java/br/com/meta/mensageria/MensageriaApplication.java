ec500a55f4fb8b8212db654c94a6c754dbfdc922
Leticia Nayara de Lima Costa <leticia.costa@meta.com.br>
2025-05-09 17:00:59 -0300

    segunda tentativa

--- src/main/java/br/com/meta/mensageria/MensageriaApplication.java
+++ src/main/java/br/com/meta/mensageria/MensageriaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableRabbit
+@EnableRabbit
 @SpringBootApplication(scanBasePackages = "br.com.meta")
 @EnableJpaRepositories(basePackages = "br.com.meta.repositories")
 @EntityScan(basePackages = "br.com.meta.models")
