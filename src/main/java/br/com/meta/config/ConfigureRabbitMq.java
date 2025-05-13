ca5ef5881d274ccc95d881d007cc69d9c7155f3a
Leticia Nayara de Lima Costa <leticia.costa@meta.com.br>
2025-05-13 15:33:51 -0300

    RabbitMQ finalizada

--- src/main/java/br/com/meta/config/ConfigureRabbitMq.java
+++ src/main/java/br/com/meta/config/ConfigureRabbitMq.java
@@ -1,10 +1,7 @@
 package br.com.meta.config;
 
 import br.com.meta.services.ConsumeMessageService;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -15,29 +12,10 @@ import org.springframework.context.annotation.Configuration;
 public class ConfigureRabbitMq {
 
     public static final String QUEUE_NAME = "crud-request-queue";
-    public static final String EXCHANGE_NAME = "crud-request-exchange";
-    public static final String ROUTING_KEY = "crud-request-rout-key.#";
 
     @Bean
     public Queue createQueue() {
         return new Queue(QUEUE_NAME, false);
-    }
-
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE_NAME);
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
-    }
-
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        return container;
     }
 
     @Bean
